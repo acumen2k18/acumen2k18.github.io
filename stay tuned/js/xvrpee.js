00b39599a646e1291d1060c112c8e01bfa05b1d6
acumen2k18 <acumen2k18@gmail.com>
2018-02-21 13:05:59 +0530

    stay tuned removed

--- stay tuned/js/xvrpee.js
+++
@@ -1,2 +0,0 @@
-var things = "<a id='Acumen EEE' href='https://eee.vce.ac.in' target='_blank'><span></span></a><a id='pens' href='contact-us/index.html' class='' target='_blank'><span>For details contact 9908480359</span></a>";
-document.body.innerHTML+=things;
